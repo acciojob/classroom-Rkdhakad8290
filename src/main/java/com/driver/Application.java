26b82c21b02523c7375f930eccdeaf20ae8cc051
Rahul kumar <rkdhakad8290@gmail.com>
2023-10-06 17:40:56 +0530

    update

--- src/main/java/com/driver/Application.java
+++ src/main/java/com/driver/Application.java
@@ -8,7 +8,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class Application {
 
-	//This is main class
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
